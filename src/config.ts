33c38dc6c9fe6080f8f3ef6f92d42fef82124ece
Owen Hardy <hardy.owen@gmail.com>
2024-01-19 19:39:02 +0100

    - updated README.md - fixed an issue when .env wasn't present

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import { promises } from 'fs'
-const { exists, mkdir, readdir } = promises
+const { exists, mkdir } = promises
 
 type Config = {
 REPO: string;
@@ -18,14 +18,19 @@ let UPDATE_INTERVAL = parseInt(process.env.UPDATE_INTERVAL || '')
 let PUSHBULLET_TOKEN = process.env.PUSHBULLET_TOKEN || ''
 
 
-const file = Bun.file('.env')
-const text = await file.text()
-const env = dotenv.parse<Config>(text)
-if (!REPO) REPO = env.REPO
-if (!GH_PAT) GH_PAT = env.GH_PAT
-if (!DATE_REGEX) DATE_REGEX = env.DATE_REGEX
-if (!PUSHBULLET_TOKEN) PUSHBULLET_TOKEN = env.PUSHBULLET_TOKEN
-if (!UPDATE_INTERVAL) UPDATE_INTERVAL = parseInt(env.UPDATE_INTERVAL)
+try {
+    const file = Bun.file('.env')
+    const text = await file.text()
+    const env = dotenv.parse<Config>(text)
+    if (!REPO) REPO = env.REPO
+    if (!GH_PAT) GH_PAT = env.GH_PAT
+    if (!DATE_REGEX) DATE_REGEX = env.DATE_REGEX
+    if (!PUSHBULLET_TOKEN) PUSHBULLET_TOKEN = env.PUSHBULLET_TOKEN
+    if (!UPDATE_INTERVAL) UPDATE_INTERVAL = parseInt(env.UPDATE_INTERVAL)
+} catch (e) {
+    console.log('no .env file found, only using process.env');
+}
+
 if (!UPDATE_INTERVAL) UPDATE_INTERVAL = 1000 * 60 * 60 // 1 hour
 
 if (!await exists(REPO_DIR.name!)) {
