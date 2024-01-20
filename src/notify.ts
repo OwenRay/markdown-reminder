46053cbe1653f8eeb8e820ba0eab488d4a6ba542
Owen Hardy <hardy.owen@gmail.com>
2024-01-20 08:55:55 +0100

    using fetch in favor of curl

--- src/notify.ts
+++ src/notify.ts
@@ -1,7 +1,21 @@
 import {PUSHBULLET_TOKEN} from './config'
 
-export function notify (s: string) {
-    Bun.spawnSync(
-        ['curl', '-u', `${PUSHBULLET_TOKEN}:`, '-d', `type=note&title=Reminder&body=${s}`, 'https://api.pushbullet.com/v2/pushes']
-    )
+export async function notify (reminderText: string) {
+
+    const response = await fetch('https://api.pushbullet.com/v2/pushes', {
+        method: 'POST',
+        headers: {
+            'Access-Token': PUSHBULLET_TOKEN,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            type: 'note',
+            title: 'Reminder',
+            body: reminderText
+        })
+    });
+
+    if (!response.ok) {
+        console.warn('Failed to send notification', response.statusText);
+    }
 }
